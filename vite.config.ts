321cd2eb078905ba33b6ef3a1b9b70b47250f061
Silvia Rodríguez Bares <uo271490@uniovi.es>
2025-06-25 19:34:17 +0200

    Trying with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'src/routes/api/upload/ocr',
+					dest: 'server/entries/endpoints/api/upload' // This will copy to .svelte-kit/output/server/entries/endpoints/api/upload
+				}
+			]
+		})
+	],
 	resolve: {
 		alias: {
 			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
